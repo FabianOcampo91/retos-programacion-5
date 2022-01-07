c159a8c77409d5f9b7dabb6a29916a3d5c789f8c
FabianOcampo91 <faby.ocampo91@gmail.com>
2022-01-07 20:43:12 -0300

    retos de progrmacion 5

--- retos.js
+++ retos.js
@@ -3,16 +3,19 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array.pop();
 }
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 function incrementarPorUno(array) {
@@ -20,12 +23,15 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  return array.map(elem => elem +  1);
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -33,6 +39,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento); //retorna un nuevo length
+  return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -41,41 +49,62 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join(' ');
 }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  return array.includes(elemento);
+  // if (array.indexOf(elemento) !== -1) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  return numeros.reduce((a, b) => a + b);
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  return promedio = resultadosTest.reduce((acc, item) => acc + item) / resultadosTest.length;
 }
 
-function numeroMasGrande(numeros) {
+function numeroMasGrande(numeros) { //(...numeros) operador rest => transforma en un array
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  return Math.max(...numeros); 
 }
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length === 0) {
+    return 0;
+  } else {
+    const array = [];
+    for (let i = 0; i < arguments.length; i++) {
+      array.push(arguments[i])
+      
+    }
+    return array.reduce ((acc, item) => acc * item);
+  }
 }
 
 function cuentoElementos(arreglo) {
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  return arreglo.filter((item) => item > 18).length; //encadenamiento demetodos
 }
 
 function diaDeLaSemana(numeroDeDia) {
@@ -83,18 +112,26 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario.
   //Escribe tu código aquí
+  
+  if (numeroDeDia === 1 || numeroDeDia === 7) {
+    return 'Es fin de semana';
+  } else {
+    return 'Es dia laboral';
+  }
 }
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  return n.toString().charAt(0) === '9';
 }
 
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí
+  return arreglo.every((item) => item === arreglo[0]);
 }
 
 function mesesDelAño(array) {
@@ -102,12 +139,19 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  const resultado = array.filter((mes) => {
+    if (mes === 'Enero' || mes === 'Marzo' || mes === 'Noviembre') {
+      return mes;
+    }
+  });
+  return resultado.length !== 3 ? 'NO se encontraron los pedidos' : resultado;
 }
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  return array.filter((num) => num > 100);
 }
 
 function breakStatement(numero) {
@@ -118,6 +162,21 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  if (numero < 0) {
+    return 'Se interrumpió la ejecución'
+  }
+  const array = [];
+  let bandera = false;
+  for (let i = 0; i < 10; i++) {
+    if ((numero + 2) === i) {
+      bandera = true;
+      break;
+    } else {
+      numero += 2;
+      array.push(numero);
+    }    
+  }
+  return array;
 }
 
 function continueStatement(numero) {
@@ -127,6 +186,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  const array = [];
+  for (let i = 1; i < 10; i++) {
+    if (i === 5) {
+      continue;
+    } else {
+      numero +=2;
+      array.push(numero);
+    }
+    
+  }
+  return array;
 }
 
 // No modificar nada debajo de esta línea
